7c0405d849ed7f1907908904c3dac9f8a6830c93
jacobo-mc <25rogers.jacob@rrcs.org>
2021-12-16 15:31:45 +0000

    update build system

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
+cd src/
+find . -name "*.class" -type f -delete
+cd ..
+mkdir temp
+cd temp
+cp -r ../src/* .
 find -name "*.java" > sources.txt
 javac @sources.txt
-cd src/
 zip -r minecraft_server.jar ./*
 cd ..
 mkdir bin
-mv src/minecraft_server.jar bin
+mv temp/minecraft_server.jar bin
+rm -rf temp
